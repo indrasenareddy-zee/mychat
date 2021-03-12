3c2c7c52ed32a36e72220575c493810293a26a1b
indrasenareddy-zee <indrasena.reddy@zee.com>
2021-03-12 18:36:21 +0530

    indra

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,6 +2,6 @@ var express = require("express")
 var router = express.Router()
 var {newUser,login} = require("../controllers/userController")
 router.post("/welcome",newUser)
-router.post("/login",login)
+router.post("/dashboard",login)
 
 module.exports = router
